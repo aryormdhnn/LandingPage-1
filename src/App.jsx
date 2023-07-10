3004fc19162e82aa0be7b5635f3c0fd3d7df6472
Aryo Romadhon <yoomadhon@gmail.com>
2023-07-11 03:23:40 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
 import React from 'react'
-import Header from './components/Header.jsx'
-import Hero from './components/Hero.jsx'
-import Facts from './components/Facts.jsx'
-import Feature from './components/Feature.jsx'
-import Section from './components/Section.jsx'
-import Testimonial from './components/Testimonial.jsx'
-import Blog from './components/Blog.jsx'
-import Footer from './components/Footer.jsx'
+import Index from './components/Index.jsx'
 
 
 function App() {
 
   return (
     <>
-      <Header />
-      <Hero />
-      <Facts />
-      <Feature />
-      <Section />
-      <Testimonial />
-      <Blog />
-      <Footer />
-
+      <Index />
     </>
   )
 }
---
+++ src/components/Index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import Header from './components/Header.jsx'
+import Hero from './components/Hero.jsx'
+import Facts from './components/Facts.jsx'
+import Feature from './components/Feature.jsx'
+import Section from './components/Section.jsx'
+import Testimonial from './components/Testimonial.jsx'
+import Blog from './components/Blog.jsx'
+import Footer from './components/Footer.jsx'
+
+
+const Index = () => {
+  return (
+      <div>
+    <Header />
+      <Hero />
+      <Facts />
+      <Feature />
+      <Section />
+      <Testimonial />
+      <Blog />
+      <Footer />
+    </div>
+  )
+}
+
+export default Index
