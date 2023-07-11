eba90e6e83b6520af054260281c52e00ab5781bd
Aryo Romadhon <yoomadhon@gmail.com>
2023-07-11 13:30:56 +0700

    fixing

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Nav, Button } from 'react-bootstrap';
 import '../css/Header.css';
-import logo from '../assets/logo.png';
+import logo from '../assets/Logo.png';
 
 const Header = () => {
   return (
